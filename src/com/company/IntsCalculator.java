7f2be44d6ee0bf4e5f5b2a4c506d9ed5687958b6
gigascript <gigascript@yandex.ru>
2022-08-23 18:28:30 +0700

    Добавлен дополнительный метод

--- src/com/company/IntsCalculator.java
+++ src/com/company/IntsCalculator.java
@@ -7,33 +7,27 @@ public class IntsCalculator implements Ints {
         this.target = new Calculator();
     }
 
-    @Override
-    public int sum(int arg0, int arg1) {
+    private int getCalculationResult(int arg0, int arg1, Calculator.Operation operation) {
         return (int) target.newFormula()
                 .addOperand(arg0)
                 .addOperand(arg1)
-                .calculate(Calculator
-                        .Operation.SUM)
+                .calculate(operation)
                 .result();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return getCalculationResult(arg0, arg1, Calculator.Operation.SUM);
     }
 
     @Override
     public int mult(int arg0, int arg1) {
-        return (int) target.newFormula()
-                .addOperand(arg0)
-                .addOperand(arg1)
-                .calculate(Calculator
-                        .Operation.MULT)
-                .result();
+        return getCalculationResult(arg0, arg1, Calculator.Operation.MULT);
+
     }
 
     @Override
-    public int pow(int a, int b) {
-        return (int) target.newFormula()
-                .addOperand(a)
-                .addOperand(b)
-                .calculate(Calculator
-                        .Operation.POW)
-                .result();
+    public int pow(int arg0, int arg1) {
+        return getCalculationResult(arg0, arg1, Calculator.Operation.POW);
     }
 }
